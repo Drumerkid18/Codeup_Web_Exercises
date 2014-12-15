1e30097ae3a020262de529a257bf3fe68470b9b2
Travis French <travisfrench@Traviss-MacBook-Pro.local>
2014-12-15 10:38:49 -0600

    added self opperating function

--- public/js/external.js
+++ public/js/external.js
@@ -19,6 +19,9 @@
 // 	console.log("I don't know what Indigo is");
 // }
 
+
+
+
 // (color == favorite) ? console.log("Green is my favorite color \"steven tyler voice\"" ) : console.log("Some-ting wong. Tis not my favorite color");
 
 // switch (color){
@@ -40,11 +43,89 @@
 // 	default:
 // 		console.log("I don't know anything with that color");
 // }
-// i
 
-var i = 100;
 
-for (; i > 0;) {
-    console.log(i);
-    i -= 5;
-}
+
+// var i = 100;
+
+// for (; i > 0;) {
+//     console.log(i);
+//     i -= 5;
+// }
+
+// var random = Math.floor((Math.random()*50)+1); random < 50; random++;
+// while(random % 2 == 0){
+// 	random = Math.floor((Math.random()*50)+1);
+// }
+// 	console.log('Random odd number to skip is: ' + random);
+
+// for (var i = 1; i < 50; i++) {
+// 	if (i % 2 == 0) {
+// 	continue;
+// 	}
+// 	if (i == random) {
+// 		console.log('Yipes! Skipping number: ' + i);
+// 	}else{
+// 		console.log('Here is an odd number: ' + i);
+// 	}
+// }
+
+
+
+// var myNameIs = 'Trav'; 
+
+// function sayHello(someting){
+// 	console.log('Hello from ' + someting);
+	
+// }
+
+// sayHello(myNameIs);
+
+// var random = Math.floor((Math.random()*100)+1);
+
+// function isOdd(number){
+// 	odd = (random % 2 == 0) ? console.log(number + ' is even') : console.log(number + ' is odd')
+// 	return odd;
+// }
+
+// isOdd(random);
+
+
+
+
+// do{
+// 	var response = prompt('What is your name?');
+	
+
+// 	alert('Ello ' + response + ' welcome to my home.');
+
+// 	do{
+// 		var pizza = prompt('Do you like pizza? ' + 'Acceptable responses are "yes" or "no"');
+// 		if (pizza == 'yes'){
+// 			alert("That is the Bee's knee's");
+// 			break;
+// 		}else if (pizza == 'no'){
+// 			alert("You are a lame person");
+// 			break;
+// 		}else {
+// 			alert('You make me upset! Lets try this again...');
+// 		}
+// 	}while (pizza !== 'yes' || pizza !== 'no');
+// } while (response == '');
+
+
+
+
+(function (){
+	var myNameIs = 'Trav'; 
+	console.log('Hello from ' + myNameIs);
+	
+})();
+
+
+(function (){
+var random = Math.floor((Math.random()*100)+1);
+(random % 2 == 0) ? console.log(random + ' is even') : console.log(random + ' is odd');
+})();
+
+
