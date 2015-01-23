5ce3c00716f4139a359292e63daa822e73165c0e
Travis French <travster217@gmail.com>
2015-01-23 17:08:56 -0600

    updated with prepare

--- park_seeder.php
+++ park_seeder.php
@@ -6,23 +6,30 @@ define('DB_PASS', 'parks_user');
 require_once('db_connect.php');
 
 $parks = [
-    ['name' => 'Big Bend', 'location' => 'Texas', 'date_established' => '1944-06-12', 'area_in_acres' => '801163.21',],
-    ['name' => 'Biscayne', 'location' => 'Florida', 'date_established' => '1980-06-28', 'area_in_acres' => '172924.07',],
-    ['name' => 'Carlsbad Caverns', 'location' => 'New Mexico', 'date_established' => '1930-05-14, ', 'area_in_acres' => '46766.45',],
-    ['name' => 'Crater Lake', 'location' => 'Oregon', 'date_established' => '1902-05-22', 'area_in_acres' => '183224.05',],
-    ['name' => 'Mesa Verde', 'location' => 'Colorado', 'date_established' => '1906-June-29', 'area_in_acres' => '52121.93',],
-    ['name' => 'Redwood', 'location' => 'California', 'date_established' => '1968-10-02, ', 'area_in_acres' => '112512.05',],
-    ['name' => 'Yosemite', 'location' => 'California', 'date_established' => '1890-10-01, ', 'area_in_acres' => '761266.19',],
-    ['name' => 'Zion', 'location' => 'Utah', 'date_established' => '1919-11-19, ', 'area_in_acres' => '146597.60',],
-    ['name' => 'Yellowstone', 'location' => 'Wyoming', 'date_established' => '1872-03-01, ', 'area_in_acres' => '2219790.71',],
-    ['name' => 'Badlands', 'location' => 'South Dakota', 'date_established' => '1978-11-10, ', 'area_in_acres' => '242755.94',],
+    ['name' => 'Big Bend', 'location' => 'Texas', 'date_established' => '1944-06-12', 'area_in_acres' => '801163.21', 'description' => 'Named for the prominent bend in the Rio Grande along the US–Mexico border, this park encompasses a large and remote part of the Chihuahuan Desert. Its main attraction is backcountry recreation in the arid Chisos Mountains and in canyons along the river. A wide variety of Cretaceous and Tertiary fossils as well as cultural artifacts of Native Americans also exist within its borders.'],
+    ['name' => 'Biscayne', 'location' => 'Florida', 'date_established' => '1980-06-28', 'area_in_acres' => '172924.07', 'description' => 'Located in Biscayne Bay, this park at the north end of the Florida Keys has four interrelated marine ecosystems: mangrove forest, the Bay, the Keys, and coral reefs. Threatened animals include the West Indian Manatee, American crocodile, various sea turtles, and peregrine falcon.'],
+    ['name' => 'Carlsbad Caverns', 'location' => 'New Mexico', 'date_established' => '1930-05-14, ', 'area_in_acres' => '46766.45', 'description' => 'Carlsbad Caverns has 117 caves, the longest of which is over 120 miles (190 km) long. The Big Room is almost 4,000 feet (1,200 m) long, and the caves are home to over 400,000 Mexican Free-tailed Bats and sixteen other species. Above ground are the Chihuahuan Desert and Rattlesnake Springs.'],
+    ['name' => 'Crater Lake', 'location' => 'Oregon', 'date_established' => '1902-05-22', 'area_in_acres' => '183224.05', 'description' => 'Crater Lake lies in the caldera of an ancient volcano called Mount Mazama that collapsed 7,700 years ago. It is the deepest lake in the United States and is famous for its vivid blue color and water clarity. There are two more recent volcanic islands in the lake, and, with no inlets or outlets, all water comes through precipitation.'],
+    ['name' => 'Mesa Verde', 'location' => 'Colorado', 'date_established' => '1906-June-29', 'area_in_acres' => '52121.93', 'description' => 'This area constitutes over 4,000 archaeological sites of the Ancestral Puebloan people, who lived here and elsewhere in the Four Corners region for at least 700 years. Cliff dwellings built in the 12th and 13th centuries include the famous Cliff Palace, which has 150 rooms and 23 kivas, and the Balcony House, with its many passages and tunnels.'],
+    ['name' => 'Redwood', 'location' => 'California', 'date_established' => '1968-10-02, ', 'area_in_acres' => '112512.05', 'description' => 'This park and the co-managed state parks protect almost half of all remaining Coastal Redwoods, the tallest trees on Earth. There are three large river systems in this very seismically active area, and 37 miles (60 km) of protected coastline reveal tide pools and seastacks. The prairie, estuary, coast, river, and forest ecosystems contain a huge variety of animal and plant species.'],
+    ['name' => 'Yosemite', 'location' => 'California', 'date_established' => '1890-10-01, ', 'area_in_acres' => '761266.19', 'description' => 'Among the earliest candidates for National Park status, Yosemite features towering granite cliffs, dramatic waterfalls, and old-growth forests at a unique intersection of geology and hydrology. Half Dome and El Capitan rise from the park\'s centerpiece, the glacier-carved Yosemite Valley, and from its vertical walls drop Yosemite Falls, North America\'s tallest waterfall. Three Giant Sequoia groves, along with a pristine wilderness in the heart of the Sierra Nevada, are home to an abundance of rare plant and animal species.'],
+    ['name' => 'Zion', 'location' => 'Utah', 'date_established' => '1919-11-19, ', 'area_in_acres' => '146597.60', 'description' => 'Located at the junction of the Colorado Plateau, Great Basin, and Mojave Desert, this geological wonder has colorful sandstone canyons, mountainous mesas, and countless rock towers. Natural arches and exposed plateau formations compose a large wilderness roughly divided into four ecosystems: desert, riparian, woodland, and coniferous forest.'],
+    ['name' => 'Yellowstone', 'location' => 'Wyoming', 'date_established' => '1872-03-01, ', 'area_in_acres' => '2219790.71', 'description' => 'Situated on the Yellowstone Caldera, the park has an expansive network of geothermal areas including vividly colored hot springs, boiling mud pots, and regularly erupting geysers, the best-known being Old Faithful and Grand Prismatic Spring. The yellow-hued Grand Canyon of the Yellowstone River has a number of scenic waterfalls, and four mountain ranges run through the park. More than 60 mammal species, including the gray wolf, grizzly bear, lynx, bison, and elk, make this park one of the best wildlife viewing spots in the country.'],
+    ['name' => 'Badlands', 'location' => 'South Dakota', 'date_established' => '1978-11-10, ', 'area_in_acres' => '242755.94', 'description' => 'The Badlands are a collection of buttes, pinnacles, spires, and grass prairies. It has the world\'s richest fossil beds from the Oligocene epoch, and the wildlife includes bison, bighorn sheep, black-footed ferrets, and swift foxes.'],
     
 ];
 
-foreach ($parks as $park) {
-    $query = "INSERT INTO national_parks (name, location, date_established, area_in_acres) VALUES ('{$park['name']}', '{$park['location']}', '{$park['date_established']}', {$park['area_in_acres']})";
+$stmt = $dbc->prepare('INSERT INTO national_parks (name, location, date_established, area_in_acres, description) VALUES (:name, :location, :date_established, :area_in_acres, :description)');
 
-    $dbc->exec($query);
+
+foreach ($parks as $park) {
+	$stmt->bindValue(':name', $park['name'], PDO::PARAM_STR);
+    $stmt->bindValue(':location',  $park['location'],  PDO::PARAM_STR);
+    $stmt->bindValue(':date_established', $park['date_established'], PDO::PARAM_STR);
+    $stmt->bindValue(':area_in_acres',  $park['area_in_acres'],  PDO::PARAM_STR);
+    $stmt->bindValue(':description',  $park['description'],  PDO::PARAM_STR);
+
+    $stmt->execute();
 
     echo "Inserted ID: " . $dbc->lastInsertId() . PHP_EOL;
 }
