0b3a41c4c6cf140531bf0f149132c84088f3139a
Travis French <travster217@gmail.com>
2015-07-23 09:33:23 -0500

    updated to not alllow none users to get in

--- public/authorized.php
+++ public/authorized.php
@@ -1,9 +1,18 @@
 <?php
+session_start();
+
+if (!$_SESSION['LOGGED_IN_USER']) {
+		header('Location: login.php');
+		exit();
+	}
 
 ?>
 
 <html>
 	<body>
 		<h1>Authorized</h1>
+		<h1> <?= $_SESSION['LOGGED_IN_USER'] ?></h1>
+		<h1> <?= $_SESSION['sessionId'] ?></h1>
+		<a href="logout.php">Logout</a>
 	</body>
 </html>
--- public/login.php
+++ public/login.php
@@ -1,22 +1,45 @@
 <?php
 
+session_start();
+$_SESSION['sessionId'] = session_id();
+
+var_dump($_SESSION['sessionId']);
+
+if (isset($_SESSION['LOGGED_IN_USER'])) {
+	if ($_SESSION['LOGGED_IN_USER']) {
+		header('Location: authorized.php');
+		exit();
+	}
+}else{
+	$_SESSION['LOGGED_IN_USER'] = false;
+}
+
 function pageController()
 {
 	$data = [];
 
+
 	$data['username'] = (!empty($_POST['username'])) ? $_POST['username'] : ' ';
 	$data['password']  = (!empty($_POST['password'])) ? $_POST['password'] : ' ';
 	
+	var_dump($data['username']);
 
 	if (($data['username'] == "username") && ($data['password'] == "password")) {
-		header('Location: /authorized.php');
+		$_SESSION['LOGGED_IN_USER'] = true;
+
 	}elseif ((!empty($_POST) && ($data['username'] != "username")) || (!empty($_POST) && ($data['password'] != "password"))) {
 		echo "please enter valid Username and Password";
+		$_SESSION['LOGGED_IN_USER'] = false;
+	}
+	if ($_SESSION['LOGGED_IN_USER']) {
+		header('Location: authorized.php');
+		exit();
 	}
 
 	return $data;
 
 }
+
 
 extract(pageController());
 
